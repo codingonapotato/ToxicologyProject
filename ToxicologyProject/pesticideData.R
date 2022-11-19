b52824526d3db913a1833c3466082b2b906c3cfb
codingonapotato <hcpc215@gmail.com>
2022-11-19 11:38:18 -0800

    Completed the code pipeline that automates the WHO classification by LD50 values

--- ToxicologyProject/pesticideData.R
+++ ToxicologyProject/pesticideData.R
@@ -1,17 +1,16 @@
 library(tidyverse)
 library(readxl)
+library(purrr)
 
 annualData <- read.csv("ToxicologyProject/data/Top_Pesticide_Use_Annual.csv")
 uniqueCompounds <- read.csv("ToxicologyProject/data/UniqueCompounds.csv")
-collected <- read_excel("ToxicologyProject/data/Toxicology.xlsx")
+collected <- read_excel("ToxicologyProject/data/NIH_Toxicology.xlsx")
 
 # Convert to excel to tibble
 collected_tibble <- as_tibble(collected)
+# print(collected_tibble)
 
-data2015 <- annualData %>%
-    filter(YEAR == 2017)
-
-## EFFECTS: returns a vector of equal size containing the WHO_Classification for a consumed vector
+## EFFECTS: returns the WHO class based on a toxicity value. Else, return NA
 ## NA if the element == NA
 ld50_to_who_class <- function(tox) {
     if (tox > 5000) {
@@ -31,28 +30,33 @@ ld50_to_who_class <- function(tox) {
 }
 
 ld50 <- collected_tibble %>%
-        select("Rat_Oral_LD50(mg/kg)") 
-print(ld50)
+        pull("Rat_Oral_LD50(mg/kg)") 
+
+WHO_automated <- new_vector <- c()    
+for (e in ld50) {
+    WHO_automated <- c(WHO_automated, ld50_to_who_class(e))
+}
 
 
-# WHO_classification <- c("III", "III", "III", NA, "III", "II", "III", NA, "II", NA)
+pesticideClasses <- collected_tibble %>%
+    bind_cols(WHO_automated) %>%
+    rename("WHOHazardClass" = ...5) %>%
+    rename("Rat_Oral_LD50" = "Rat_Oral_LD50(mg/kg)") %>%
+    rename("Compound" = "Pesticide/Compound") %>%
+    mutate("WHOHazardClass" = as.factor(WHOHazardClass)) %>%
+    arrange(Rat_Oral_LD50)
 
-# pesticideClasses <- data2015 %>%
-#     bind_cols(WHO_classification) %>%
-#     rename("WHOHazardClass" = ...4) %>%
-#     mutate("WHOHazardClass" = as.factor(WHOHazardClass))
-
-# print(pesticideClasses)
+print(pesticideClasses)
 
 
-# pesticideBarGraph <- pesticideClasses %>%
-#                         ggplot(aes(x = AnnualUseKG, y = reorder(COMPOUND, AnnualUseKG), fill = WHOHazardClass)) +
-#                         geom_bar(stat = "identity") +
-#                         labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Hazard Class") +
-#                         ggtitle("Pesticide Hazard Classification with Sorted in Descending Order of Annual Use (Kg)") +
-#                         theme(text = element_text(size = 20))
+pesticideBarGraph <- pesticideClasses %>%
+                        ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = WHOHazardClass)) +
+                        geom_bar(stat = "identity") +
+                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Hazard Class") +
+                        ggtitle("Pesticide Hazard Classification with Sorted in Descending Order of Annual Use (Kg)") +
+                        theme(text = element_text(size = 20))
                         
-# print(pesticideBarGraph)
+print(pesticideBarGraph)
 
 # ggsave("PesticideClassGraph.jpeg", 
 #         plot = pesticideBarGraph,
@@ -67,4 +71,4 @@ print(ld50)
 ## Excel: Pesticide name, Data, Species, Link to paper/Pseudo-citation
 ## Web-scraping for the all pesticides possible(?)
 ## Figure out share of annual pesticide use as percentage of the whole 
-## Set arbitrary threshold 
+## Set arbitrary threshold
