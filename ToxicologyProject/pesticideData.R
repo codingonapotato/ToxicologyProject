c9c27d470010811620bc466cf9724278af13cb0d
codingonapotato <hcpc215@gmail.com>
2022-11-19 01:14:15 -0800

    Added a new function that will (hopefully) serve as a helper function to iteratively or recursively operate on... either a tibble, list, or vector TBD!

--- ToxicologyProject/pesticideData.R
+++ ToxicologyProject/pesticideData.R
@@ -1,40 +1,59 @@
 library(tidyverse)
-# Loaded libraries
+library(readxl)
 
-annualData <- read.csv("./data/Top_Pesticide_Use_Annual.csv")
+annualData <- read.csv("ToxicologyProject/data/Top_Pesticide_Use_Annual.csv")
+uniqueCompounds <- read.csv("ToxicologyProject/data/UniqueCompounds.csv")
+collected <- read_excel("ToxicologyProject/data/Toxicology.xlsx")
+
+# Convert to excel to tibble
+collected_tibble <- as_tibble(collected)
 
 data2015 <- annualData %>%
     filter(YEAR == 2017)
 
-WHO_classification <- c("III", "III", "III", NA, "III", "II", "III", NA, "II", NA)
-# Data attribution: The WHO Recommended Classification of Pesticides by Hazard
-# and Guides to Classification 2019
-# For data I could not find, I used NA values as placeholders
+## EFFECTS: returns a vector of equal size containing the WHO_Classification for a consumed vector
+## NA if the element == NA
+ld50_to_who_class <- function(tox) {
+    if (tox > 5000) {
+        a <- "U"
+    } else if (tox > 2000) {
+       a <- "III"
+    } else if (tox >= 50) {
+       a <- "II"
+    } else if (tox >= 5) {
+       a <- "Ib"
+    } else if (tox < 5) {
+        a <- "Ia"
+    } else {
+        a <- NA
+    }
+    a
+}
 
-## TODO: ASK IF I SHOULD TRY AND LOOK AT ONLINE DATABASES LIKE ECOTOX TO TRY
-## AND CLASSIFY USING WHO GUIDELINES USING ORAL LD50 DATA
+ld50 <- collected_tibble %>%
+        select("Rat_Oral_LD50(mg/kg)") 
+print(ld50)
 
-## TODO2: PETROLEUM IS A PESTICIDE? YES 
 
-## TODO3: HOW TO AGGREGATE DATA IF MODEL ORGANISMS NOT-STANDARDIZED?
-## Citation manager to keep track of all the sources if need to aggregate multiple sources?
+# WHO_classification <- c("III", "III", "III", NA, "III", "II", "III", NA, "II", NA)
 
-pesticideClasses <- data2015 %>%
-    bind_cols(WHO_classification) %>%
-    rename("WHOHazardClass" = ...4) %>%
-    mutate("WHOHazardClass" = as.factor(WHOHazardClass))
+# pesticideClasses <- data2015 %>%
+#     bind_cols(WHO_classification) %>%
+#     rename("WHOHazardClass" = ...4) %>%
+#     mutate("WHOHazardClass" = as.factor(WHOHazardClass))
 
 # print(pesticideClasses)
 
 
-pesticideBarGraph <- pesticideClasses %>%
-                        ggplot(aes(x = AnnualUseKG, y = reorder(COMPOUND, AnnualUseKG), fill = WHOHazardClass)) +
-                        geom_bar(stat = "identity") +
-                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Hazard Class") +
-                        ggtitle("Pesticide Hazard Classification with Sorted in Descending Order of Annual Use (Kg)") +
-                        theme(text = element_text(size = 20))
+# pesticideBarGraph <- pesticideClasses %>%
+#                         ggplot(aes(x = AnnualUseKG, y = reorder(COMPOUND, AnnualUseKG), fill = WHOHazardClass)) +
+#                         geom_bar(stat = "identity") +
+#                         labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Hazard Class") +
+#                         ggtitle("Pesticide Hazard Classification with Sorted in Descending Order of Annual Use (Kg)") +
+#                         theme(text = element_text(size = 20))
                         
-print(pesticideBarGraph)
+# print(pesticideBarGraph)
+
 # ggsave("PesticideClassGraph.jpeg", 
 #         plot = pesticideBarGraph,
 #         device = "jpeg",
@@ -42,8 +61,8 @@ print(pesticideBarGraph)
 #         height = 10,
 #         path = "./")
 
-## TODO4: ASK ABOUT HOW TO CHANGE PLOT SCALE AND STUFF ON VSCODE
 
+## TO DO:
 
 ## Excel: Pesticide name, Data, Species, Link to paper/Pseudo-citation
 ## Web-scraping for the all pesticides possible(?)
