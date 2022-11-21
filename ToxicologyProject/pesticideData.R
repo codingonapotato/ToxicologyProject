510bb3f15c19c2a0e300ebf4398e33cc06e1d942
codingonapotato <hcpc215@gmail.com>
2022-11-20 19:28:51 -0800

    Added data for a few more pesticides and added visuals to be used during Monday's meeting

--- ToxicologyProject/pesticideData.R
+++ ToxicologyProject/pesticideData.R
@@ -1,10 +1,13 @@
 library(tidyverse)
 library(readxl)
+library(xlsx)
 library(purrr)
+library(ggpubr)
 
-annualData <- read.csv("ToxicologyProject/data/Top_Pesticide_Use_Annual.csv")
-uniqueCompounds <- read.csv("ToxicologyProject/data/UniqueCompounds.csv")
-collected <- read_excel("ToxicologyProject/data/NIH_Toxicology.xlsx")
+annualData <- read.csv("./data/Top_Pesticide_Use_Annual.csv")
+uniqueCompounds <- read.csv("./data/UniqueCompounds.csv")
+collected <- read_excel("./data/NIH_Toxicology.xlsx")
+# full_data <- read.csv("ToxicologyProject/data/Pesticidedata_Full.csv")
 
 # Convert to excel to tibble
 collected_tibble <- as_tibble(collected)
@@ -44,6 +47,7 @@ pesticideClasses <- collected_tibble %>%
     rename("Rat_Oral_LD50" = "Rat_Oral_LD50(mg/kg)") %>%
     rename("Compound" = "Pesticide/Compound") %>%
     mutate("WHOHazardClass" = as.factor(WHOHazardClass)) %>%
+    mutate("Proportion_of_Total_Use" = Rat_Oral_LD50 / sum(AnnualUseKG)) %>%
     arrange(Rat_Oral_LD50)
 
 print(pesticideClasses)
@@ -52,19 +56,64 @@ print(pesticideClasses)
 pesticideBarGraph <- pesticideClasses %>%
                         ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = WHOHazardClass)) +
                         geom_bar(stat = "identity") +
-                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Hazard Class") +
-                        ggtitle("Pesticide Hazard Classification with Sorted in Descending Order of Annual Use (Kg)") +
-                        theme(text = element_text(size = 20))
-                        
+                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "WHO Hazard Class") +
+                        theme(text = element_text(size = 20)) +
+                        ggtitle("Annual Pesticide Usage (Kg) in Descending Order by WHO Hazard Class")
 print(pesticideBarGraph)
 
-# ggsave("PesticideClassGraph.jpeg", 
-#         plot = pesticideBarGraph,
+pesticideBarGraphClassII <- pesticideClasses %>%
+                        filter(WHOHazardClass == "II") %>%
+                        ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = Compound)) +
+                        geom_bar(stat = "identity") +
+                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Compound") +
+                        theme(text = element_text(size = 20))
+                        
+# print(pesticideBarGraphClassII)
+
+pesticideBarGraphClassIII <- pesticideClasses %>%
+                        filter(WHOHazardClass == "III") %>%
+                        ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = Compound)) +
+                        geom_bar(stat = "identity") +
+                        labs(x = "Annual Use (KG)", y = "", fill = "Compound") +
+                        theme(text = element_text(size = 20))
+                        
+# print(pesticideBarGraphClassIII)
+
+BothClass <- ggarrange(pesticideBarGraphClassII, pesticideBarGraphClassIII, ncol = 2, nrow = 1, legend = "bottom") %>%
+    annotate_figure(top = text_grob("Annual Pesticide Usage (Kg) in Descending Order for WHO Hazard Class II and III (U-omitted)", color = 'red', face = "bold", size = 20))
+
+proportion_bar <- pesticideClasses %>%
+    group_by(WHOHazardClass) %>%
+    ggplot(aes(y = AnnualUseKG, x = WHOHazardClass, fill = Compound)) +
+    geom_bar(position = "stack", stat = "identity") +
+    facet_grid(~ WHOHazardClass, scale = "free") +
+    theme(text = element_text(size = 30)) +
+    ggtitle("Average Relative Proportion of Pesticide Compound Used By WHO Hazard Class")
+
+# print(proportion_bar)
+
+# ggsave("Proportion_Bar.jpeg", 
+#         plot = proportion_bar,
 #         device = "jpeg",
-#         width = 50,
-#         height = 10,
+#         width = 20,
+#         height = 18,
 #         path = "./")
 
+ggsave("PesticideClassGraph.jpeg", 
+        plot = pesticideBarGraph,
+        device = "jpeg",
+        width = 20,
+        height = 18,
+        path = "./")
+
+# Can write this to file for quicker/easier access
+# full_2017 <- full_data %>%
+#     filter(YEAR == 2017) %>%
+#     group_by(COMPOUND) %>%
+#     summarize("Average_High_KG" = mean(EPEST_HIGH_KG)) %>%
+#     arrange(desc(Average_High_KG)) %>%
+#     head(20)
+# print(full_2017)
 
 ## TO DO:
 
