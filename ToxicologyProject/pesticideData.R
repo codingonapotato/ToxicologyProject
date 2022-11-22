146c6958e425dccb356815da63116a1dfdba2864
codingonapotato <hcpc215@gmail.com>
2022-11-21 16:18:16 -0800

    added new code pipeline for side-by-side graphic

--- ToxicologyProject/pesticideData.R
+++ ToxicologyProject/pesticideData.R
@@ -2,10 +2,11 @@ library(tidyverse)
 library(readxl)
 library(xlsx)
 library(purrr)
+library(ggpubr)
 
-annualData <- read.csv("ToxicologyProject/data/Top_Pesticide_Use_Annual.csv")
-uniqueCompounds <- read.csv("ToxicologyProject/data/UniqueCompounds.csv")
-collected <- read_excel("ToxicologyProject/data/NIH_Toxicology.xlsx")
+annualData <- read.csv("./data/Top_Pesticide_Use_Annual.csv")
+uniqueCompounds <- read.csv("./data/UniqueCompounds.csv")
+collected <- read_excel("./data/NIH_Toxicology.xlsx")
 # full_data <- read.csv("ToxicologyProject/data/Pesticidedata_Full.csv")
 
 # Convert to excel to tibble
@@ -55,25 +56,55 @@ print(pesticideClasses)
 pesticideBarGraph <- pesticideClasses %>%
                         ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = WHOHazardClass)) +
                         geom_bar(stat = "identity") +
-                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Hazard Class") +
-                        ggtitle("Pesticide Hazard Classification with Sorted in Descending Order of Annual Use (Kg)") +
+                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "WHO Hazard Class") +
+                        theme(text = element_text(size = 20)) +
+                        ggtitle("Annual Pesticide Usage (Kg) in Descending Order by WHO Hazard Class")
+print(pesticideBarGraph)
+
+pesticideBarGraphClassII <- pesticideClasses %>%
+                        filter(WHOHazardClass == "II") %>%
+                        ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = Compound)) +
+                        geom_bar(stat = "identity") +
+                        labs(x = "Annual Use (KG)", y = "Pesticide", fill = "Compound") +
                         theme(text = element_text(size = 20))
                         
-# print(pesticideBarGraph)
+# print(pesticideBarGraphClassII)
 
-proprtion_bar <- pesticideClasses %>%
+pesticideBarGraphClassIII <- pesticideClasses %>%
+                        filter(WHOHazardClass == "III") %>%
+                        ggplot(aes(x = AnnualUseKG, y = reorder(Compound, AnnualUseKG), fill = Compound)) +
+                        geom_bar(stat = "identity") +
+                        labs(x = "Annual Use (KG)", y = "", fill = "Compound") +
+                        theme(text = element_text(size = 20))
+                        
+# print(pesticideBarGraphClassIII)
+
+BothClass <- ggarrange(pesticideBarGraphClassII, pesticideBarGraphClassIII, ncol = 2, nrow = 1, legend = "bottom") %>%
+    annotate_figure(top = text_grob("Annual Pesticide Usage (Kg) in Descending Order for WHO Hazard Class II and III (U-omitted)", color = 'red', face = "bold", size = 20))
+
+proportion_bar <- pesticideClasses %>%
     group_by(WHOHazardClass) %>%
     ggplot(aes(y = AnnualUseKG, x = WHOHazardClass, fill = Compound)) +
-    geom_bar(position = "stack", stat = "identity")
+    geom_bar(position = "stack", stat = "identity") +
+    facet_grid(~ WHOHazardClass, scale = "free") +
+    theme(text = element_text(size = 30)) +
+    ggtitle("Average Relative Proportion of Pesticide Compound Used By WHO Hazard Class")
 
-# print(proprtion_bar)
+# print(proportion_bar)
 
-# ggsave("PesticideClassGraph.jpeg", 
-#         plot = pesticideBarGraph,
+# ggsave("Proportion_Bar.jpeg", 
+#         plot = proportion_bar,
 #         device = "jpeg",
-#         width = 50,
-#         height = 10,
+#         width = 20,
+#         height = 18,
 #         path = "./")
+
+ggsave("PesticideClassGraph.jpeg", 
+        plot = pesticideBarGraph,
+        device = "jpeg",
+        width = 20,
+        height = 18,
+        path = "./")
 
 # Can write this to file for quicker/easier access
 # full_2017 <- full_data %>%
